dc5766f48e0482a64c8e2652a68627bdcd13810d
Sylvia Ely <sylvianne9417@gmail.com>
2023-02-12 13:46:22 -0800

    think i figured out the reaction create and delete routes via mongoose documentation

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -91,4 +91,34 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  // Add the whole body of a reaction to a thought.
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with the id" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete whole body of reaction from thought
+  deleteReaction(req, res) {
+    Thought.findOneAndDelete(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with the id" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
